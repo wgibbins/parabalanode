f32e38766c700981ad3615918b00381e75f2e0bd
Wilson Gibbins <PostModernLuddite@gmail.com>
2015-02-12 21:25:50 -0600

    initial commit

---
+++ parabola.js
@@ -0,0 +1,17 @@
+var sys = require("sys"),  
+my_http = require("http");  
+my_http.createServer(function(request,response){  
+   // console.log("I got kicked");  
+	var parts = require('url').parse(request.url, true);
+	// console.log(JSON.stringify(parts.query.b));
+	var x = Number(parts.query.x);
+	var a = Number(parts.query.a);
+	var b = Number(parts.query.b);
+	var c = Number(parts.query.c);		
+	var y = a * x * x + b * x + c;
+	var result = [x, y];
+    response.writeHeader(200, {"Content-Type": "application/json"});  
+    response.write(JSON.stringify(result));  
+    response.end();  
+}).listen(8080);  
+console.log("Server Running on 8080");   
